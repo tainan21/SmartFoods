1ff1b3fd3d19bc20d82fd19d705f6a497db139ac
Tainan Camargo de Moraes <tainancmoraes@gmail.com>
2022-05-23 22:17:11 -0300

    Alter crud products

--- src/pages/CrudProducts.vue
+++ src/pages/CrudProducts.vue
@@ -6,38 +6,27 @@
                 <vs-input dark  color="#7d33ff" border type="text" v-model="name" placeholder="Nome do Produto">
                     <template #icon> <i class='bx bx-lock-open-alt'></i></template>
                 </vs-input>
-            </div>
-        
+            </div>  
             <div class="col-12 m-2">
-                <vs-input dark  color="#7d33ff" border type="password" v-model="description" placeholder="Nome do Produto">
+                <vs-input dark  color="#7d33ff" border type="text" v-model="description" placeholder="Descrição">
                     <template #icon> <i class='bx bx-lock-open-alt'></i></template>
                 </vs-input>
             </div>
-         
             <div class="col-12 m-2">
-                <vs-input dark  color="#7d33ff" border type="password" v-model="price" placeholder="Nome do Produto">
+                <vs-input dark  color="#7d33ff" border type="text" v-model="price" placeholder="Preço">
                     <template #icon> <i class='bx bx-lock-open-alt'></i></template>
                 </vs-input>
             </div>
-
             <div class="col-12 m-2">
-                <vs-input dark  color="#7d33ff" border type="password" v-model="category" placeholder="Nome do Produto">
+                <vs-input dark  color="#7d33ff" border type="text" v-model="category" placeholder="Categoria">
                     <template #icon> <i class='bx bx-lock-open-alt'></i></template>
                 </vs-input>
             </div>
-        
             <div class="col-12 m-2">
-                <vs-input dark  color="#7d33ff" border type="password" v-model="quantity" placeholder="Nome do Produto">
+                <vs-input dark  color="#7d33ff" border type="text" v-model="quantity" placeholder="Quantidade">
                     <template #icon> <i class='bx bx-lock-open-alt'></i></template>
                 </vs-input>
-            </div>
-         
-            <div class="col-12 m-2">
-                <vs-input dark  color="#7d33ff" border type="password" v-model="value2" placeholder="Nome do Produto">
-                    <template #icon> <i class='bx bx-lock-open-alt'></i></template>
-                </vs-input>
-            </div>
-       
+            </div>       
             <div class="col-12">Fotos aqui</div>
         </div>
     </div>
@@ -56,23 +45,6 @@
         sales: 3
       })
     }
-    /*
-          CreationDate: "2022-05-11",
-            CreationUserId: 1,
-            UpdateDate: "2022-05-11",
-            UpdateUserId: 1,
-            DeletionDate: null,
-            DeletionUserId: null,
-            code: 1,
-            name: "amazon",
-            description: "teste descrição",
-            image: "link img",
-            price: 45,
-            category: "teste",
-            quantity: 4,
-            rating: 5,
-            sales: "3"
-    */
   </script>
 
   
