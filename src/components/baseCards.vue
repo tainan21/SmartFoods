a701c548f41d18b029adfd122806f9f69f24172e
Tainan Camargo de Moraes <tainancmoraes@gmail.com>
2022-05-29 16:00:13 -0300

    NewAlters

---
+++ src/components/baseCards.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card flex flex-col bg-white dark:bg-gray-800 dark:text-white">
+        <div class="card-body"></div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.card {
+    border-radius: 10px;
+    box-shadow: 0 4px 20px 1px rgb(0 0 0 / 6%), 0 1px 4px rgb(0 0 0 / 8%);
+    // box-shadow:rgb(149 157 165 / 20%) 0px 8px 24px;
+
+    .card-body {
+        -webkit-box-flex: 1;
+        -ms-flex: 1 1 auto;
+        flex: 1 1 auto;
+        padding: 1.25rem;
+        &.noPadding {
+            padding: 0 !important;
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,105 +1,115 @@
 <template>
-  <div class="container">
-    <h1 class="Title">Painel</h1>
-    <h3 class="">Boas Vindas, GalaxiaBurguer!</h3>
-    <h3 class="">Fique ligado nas suas estátisticas!</h3>
-     <div class="tab-content tab-content-basic">
-            <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview"> 
-                <div class="row">
-                    <div class="col-sm-12">
-                        <h2> Novos Pedidos </h2>
-                        <div class="statistics-details d-flex align-items-center justify-content-between">
-                            <div>
-                                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                                    <div class="card-header statistics-title">Bounce Rate</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title rate-percentage">32.53%</h5>
-                                        <p class="card-text text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>-0.5%</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                                    <div class="card-header statistics-title">Page Views</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title rate-percentage">7,682</h5>
-                                        <p class="card-text text-danger d-flex"><i class="mdi mdi-menu-down"></i><span>+0.1%</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                                    <div class="card-header statistics-title">New Sessions</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title rate-percentage">68.8</h5>
-                                        <p class="d-flex"><i class="mdi mdi-menu-down"></i><span>68.8</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-                                    <div class="card-header statistics-title">Avg. Time on Site</div>
-                                    <div class="card-body">
-                                        <h5 class="card-title rate-percentage">2m:35s</h5>
-                                        <p class="d-flex"><i class="mdi mdi-menu-down"></i><span>+0.8%</span></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+     <div class="container">
+          <h1 class="title">Painel</h1>
+          <div class="tab-content tab-content-basic">
+               <div class="cardWrapper"> 
+                    <h2 class="subtitle"> Novos Pedidos </h2>
+                    <div class="row">
+                         <div class="col-lg-3 col-6">
+                              <div class="cardStyle">
+                                   <div class="flex align-center cardStyleBox">
+                                        <i class="bx bx-user cardStyleIcon"></i>
+                                        <div class="m-auto">
+                                             <p class="cardStyleLegenda">Novos Clientes</p>
+                                             <p class="cardStyleNumber">205</p>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                          <div class="col-lg-3 col-6">
+                              <div class="cardStyle">
+                                   <div class="flex align-center cardStyleBox">
+                                        <i class="bx bx-user cardStyleIcon"></i>
+                                        <div class="m-auto">
+                                             <p class="cardStyleLegenda">Novos Clientes</p>
+                                             <p class="cardStyleNumber">205</p>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                          <div class="col-lg-3 col-6">
+                              <div class="cardStyle">
+                                   <div class="flex align-center cardStyleBox">
+                                        <i class="bx bx-user cardStyleIcon"></i>
+                                        <div class="m-auto">
+                                             <p class="cardStyleLegenda">Novos Clientes</p>
+                                             <p class="cardStyleNumber">205</p>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                          <div class="col-lg-3 col-6">
+                              <div class="cardStyle">
+                                   <div class="flex align-center cardStyleBox">
+                                        <i class="bx bx-user cardStyleIcon"></i>
+                                        <div class="m-auto">
+                                             <p class="cardStyleLegenda">Novos Clientes</p>
+                                             <p class="cardStyleNumber">205</p>
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-  <div class="row">
-      <div class="col"> Grafico de Lucros aqui </div>
-      <div class="col"> 
-        <div class="center examplex">
-          <h1> Novos Pedidos </h1>
-        <vs-table striped>
+               </div>
+          </div>
+          <div class="row">
+          <div class="col">
+               <h2 class="subtitle">Gráfico de Lucro </h2>
+          </div>
+          <div class="col"> 
+          <div class="center examplex">
+                    <h2 class="subtitle"> Novos Pedidos </h2>
+          <vs-table striped>
           <template #thead>
-            <vs-tr>
-              <vs-th>Pedido</vs-th>
-              <vs-th>Cliente</vs-th>
-              <vs-th>Lucro</vs-th>
-              <vs-th>Status</vs-th>
-            </vs-tr>
+          <vs-tr>
+          <vs-th>Pedido</vs-th>
+          <vs-th>Cliente</vs-th>
+          <vs-th>Lucro</vs-th>
+          <vs-th>Status</vs-th>
+          </vs-tr>
           </template>
           <template #tbody>
-            <vs-tr :key="i" v-for="(tr, i) in NewPedidos" :data="tr">
-              <vs-td>{{ tr.name }}</vs-td><vs-td>{{ tr.email }}</vs-td><vs-td> R${{ tr.lucro }}</vs-td><vs-td>{{ tr.status }}</vs-td>
-            </vs-tr>
+          <vs-tr :key="i" v-for="(tr, i) in NewPedidos" :data="tr">
+          <vs-td>{{ tr.name }}</vs-td><vs-td>{{ tr.email }}</vs-td><vs-td> R${{ tr.lucro }}</vs-td><vs-td>{{ tr.status }}</vs-td>
+          </vs-tr>
           </template>
-        </vs-table>
-      </div> 
-      </div>
-  </div>
-    <div class="row">
-      <div class="col"> Gráfico de aumento de vendas  </div>
-      <div class="col">   
-        <div class="center examplex">
-        <h1> Novos Clientes </h1>
-          <vs-table striped>
-            <template #thead>
-              <vs-tr>
-                <vs-th>Name</vs-th><vs-th>Email</vs-th><vs-th>Id</vs-th>
-              </vs-tr>
-            </template>
-            <template #tbody>
-              <vs-tr :key="i" v-for="(tr, i) in Newusers" :data="tr">
-                <vs-td>{{ tr.name }}</vs-td><vs-td>{{ tr.email }}</vs-td><vs-td>{{ tr.id }}</vs-td>
-              </vs-tr>
-            </template>
           </vs-table>
-        </div>
-      </div>
-  </div>
-    <div class="row">
-      <div class="col"> Novidades do Sistema </div>
-    </div>
-  </div>
+          </div> 
+          </div>
+          </div>
+          <div class="row">
+
+               <div class="col">
+                    <h2 class="subtitle">Gráfico de aumento de vendas </h2>
+               </div>
+               <div class="col">   
+                    <div class="center examplex">
+                    <h2 class="subtitle"> Novos Cliente </h2>
+                    <vs-table striped>
+                         <template #thead>
+                         <vs-tr>
+                              <vs-th>Name</vs-th><vs-th>Email</vs-th><vs-th>Id</vs-th>
+                         </vs-tr>
+                         </template>
+                         <template #tbody>
+                              <vs-tr :key="i" v-for="(tr, i) in Newusers" :data="tr">
+                                   <vs-td>{{ tr.name }}</vs-td><vs-td>{{ tr.email }}</vs-td><vs-td>{{ tr.id }}</vs-td>
+                              </vs-tr>
+                         </template>
+                    </vs-table>
+                    </div>
+               </div>
+          </div>
+          <div class="row">
+                <div class="col">
+                    <h2 class="subtitle">Novidades do Sistema </h2>
+               </div>
+          </div>
+     </div>
 </template>
 
     
+
 <script>
     export default {
       data:() => ({
@@ -141,4 +151,46 @@
     }
     </script>
 
-        
+<style lang="scss">
+.title{
+     color: #2C3357;
+     font-size: 28px;
+     line-height: 1.3;
+   
+}
+.subtitle{
+     color: #2C3357;
+     font-size: 24px;
+     line-height: 1.3;
+}
+.cardStyle{
+     -webkit-box-flex: 1;
+     -ms-flex: 1 1 auto;
+     flex: 1 1 auto;
+     padding: 1.25rem;
+     border-radius: 10px;
+    box-shadow: 0 4px 20px 1px rgb(0 0 0 / 6%), 0 1px 4px rgb(0 0 0 / 8%);
+    background: #1f2937;
+    //#9ca3b9
+    //#8b5cf6
+    &Box{
+        flex-direction: row; 
+        display: flex;
+        align-items: center;
+    }
+    &Legenda{
+         color: #9ca3b9;
+    }
+    &Number{
+          color: #8b5cf6    
+    }
+    &Icon{
+         color: white;
+         font-size: 3rem;
+    }
+ 
+}
+.cardWrapper{
+     margin: 20px 0;
+}
+</style>
